79ba0a7d208a812d130b15dfd6336fb6ee68cb95
Hussam Abd <hussam@liliomlab.com>
2019-05-30 15:28:05 +0300

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,26 @@ require('./bootstrap');
 
 // Start Here
 
+var app = new Vue({
+    el: '#app',
+    data: {
+        headline: 'Headline',
+        subtitle: 'Subtitle',
+        bodyText: 'Body Text',
+
+        phone: '+90 553 032 04 10',
+        avatar: 'https://lh3.googleusercontent.com/-iZQ4s0YsF4Q/XCThDYSIp6I/AAAAAAAAACk/kG-_LHdY884yG5_-9XXpOVhqOxIYH1UjwCEwYBhgL/w280-h278-p/IMG_9100.jpg',
+        links: {
+            facebook: 'https://www.facebook.com/Hussam3bd',
+            twitter: 'https://twitter.com/Hussam3bd',
+            linkedin: 'https://www.linkedin.com/in/hussam3bd',
+            instagram: 'https://www.instagram.com/hussam3bd',
+        },
+    },
+    methods: {
+        supertrim(value) {
+            this.utmTerm = value.replace(/\s/g, '');
+        },
+    }
+});
+
