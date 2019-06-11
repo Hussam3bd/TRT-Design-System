c8b723c0ae4c3237a8c8f5589752c5c99a007b81
Hussam Abd <hussam3bd@gmail.com>
2019-06-11 12:06:31 +0300

    finish card component 1.2v

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,22 @@ var app = new Vue({
     el: '#app',
     data: {
         card: {
+            options: {
+                header: true,
+                footer: true,
+                thumbnail: true,
+                body: true,
+            },
             type: 'news',
             style: 'boxed',
             headline: 'Headline',
             subtitle: 'Subtitle',
             bodyText: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-            thumbnail: 'https://cdni0.trtworld.com/w480/h270/q75/56377_AUSSebastianKurz_1558969435854.JPG',
+            thumbnail: 'https://cdni0.trtworld.com/w960/h540/q75/57159_USAManhattanhelicopter_1560205472646.jpg',
             episodes: '23 Bölüm',
             duration: '45 dk',
-            CTAText: '',
-            CTAURL: '',
+            CTAText: 'Card Link',
+            CTAURL: '#!',
         }
     },
     methods: {
--- assets/js/components/CardComponent.vue
+++ assets/js/components/CardComponent.vue
@@ -2,42 +2,63 @@
 	<div :class="{
 		'components-card': true,
 		'has-thumbnail': card.thumbnail,
-		'card-boxed': card.style == 'boxed'
+		'card-boxless': card.style == 'boxless',
+		'card-overlays': card.type == 'overlays',
 	}">
-		<div class="components-card__header">
-			<div :class="{'components-card__thumbnail': true, 'has-gradient' : card.type == 'show'}">
-				<img v-show="card.thumbnail" :src="card.thumbnail" class="img-fluid" :alt="card.headline">
-			</div>
-			<a href="#!" class="components-card__support badge badge-primary">Primary Support</a>
-
-			<div class="components-card__action" v-show="card.type == 'show'">
-				<a href="#">
-					<i class="fas fa-play"></i>
-				</a>
-				<div>
-					<strong class="d-block text-primary" v-show="card.episodes">{{ card.episodes }}</strong>
-					<span class="d-block" v-show="card.duration">{{ card.duration }}</span>
+		<div class="components-card__header" v-show="card.options.header">
+			<div class="media">
+				<img class="mr-2 rounded-circle align-self-center" width="30" height="30"
+				     src="https://randomuser.me/api/portraits/women/21.jpg"
+				     alt="...">
+				<div class="media-body">
+					<h5 class="mt-1 mb-0">Rawan</h5>
 				</div>
+			</div>
+			<div class="action">
+				<a href="#!">
+					<i class="far fa-bookmark"></i>
+				</a>
 			</div>
 		</div>
 
-		<div class="components-card__body">
-			<h3 class="components-card__title" v-show="card.headline">
-				<a href="#!">{{ card.headline }}</a>
-			</h3>
-			<h5 class="components-card__subtitle" v-show="card.subtitle">{{ card.subtitle}}</h5>
-			<p class="components-card__text mb-0" v-show="card.bodyText">{{ card.bodyText }}</p>
-			<a :href="card.CTAURL" class="btn btn-primary" v-show="card.CTAText" target="_blank">{{card.CTAText}}</a>
-			<!--
-			<ul class="meta">
-				<li>
-					<a href="#">Category</a>
-				</li>
-				<li>
-					<time>Timestamp</time>
-				</li>
-			</ul>
-			-->
+		<div class="position-relative">
+
+			<div v-show="card.options.thumbnail"
+			     :class="{'components-card__thumbnail': true, 'has-gradient' : card.type == 'show', 'position-relative': true}">
+				<img v-show="card.thumbnail" :src="card.thumbnail" class="img-fluid" :alt="card.headline">
+
+				<!--<a href="#!" class="components-card__support badge badge-primary">Primary Support</a>-->
+
+				<div class="components-card__action" v-show="card.type == 'show'">
+					<a href="#">
+						<i class="fas fa-play"></i>
+					</a>
+					<div>
+						<strong class="d-block text-light fwb" v-show="card.episodes">{{ card.episodes }}</strong>
+						<span class="d-block text-light" v-show="card.duration">{{ card.duration }}</span>
+					</div>
+				</div>
+			</div>
+
+			<div v-show="card.options.body" class="components-card__body">
+				<h3 class="components-card__title" v-show="card.headline">
+					<a href="#!">{{ card.headline }}</a>
+				</h3>
+				<h5 class="components-card__subtitle" v-show="card.subtitle">{{ card.subtitle}}</h5>
+				<p class="components-card__text mb-0" v-show="card.bodyText">{{ card.bodyText }}</p>
+				<a :href="card.CTAURL" class="btn-link mt-2 d-block" v-show="card.CTAText">{{card.CTAText}}</a>
+			</div>
+
+		</div>
+
+		<div class="components-card__footer clearfix" v-show="card.options.footer">
+			<a href="#!" class="btn btn-dark float-left">Read More</a>
+
+			<div class="action">
+				<a href="#!">
+					<i class="far fa-share-square"></i>
+				</a>
+			</div>
 		</div>
 	</div>
 </template>
